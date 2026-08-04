b62d0a9e8ee29f745e1e22402b8627a490095a77
reebme <25910254+reebme@users.noreply.github.com>
2026-08-04 12:08:56 +0200

    Added functions for data import and cleaning.

---
+++ Src/preparation.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+def import_bicliques(processed_data_file, bicliques_file):
+    '''
+    processed_data_file contains the original data with missingness
+    it's a pandas dataframe written into a parquet file
+    bicliques_file contains a dataframe with found rows and columns of the complete submatrices
+    it's a csv file, parquet won't serialize numpy arrays
+    '''
+    bicliques = pd.read_csv(bicliques_file, index_col=0)
+
+    # bicliques are written in the file as strings separated with whitespace, with occasional \n
+    bicliques['found_rows'] = bicliques['found_rows'].map(
+        lambda s: np.fromstring(s.strip("[ ]").replace('\n', ''), sep=' ', dtype=int)
+    )
+    bicliques['found_cols'] = bicliques['found_cols'].map(
+        lambda s: np.fromstring(s.strip("[ ]").replace('\n', ''), sep=' ', dtype=int)
+    )
+
+    data = pd.read_parquet(processed_data_file)
+
+    complete_data = []
+    for _, bc in bicliques.iterrows():
+        rows, cols = bc[["found_rows", "found_cols"]]
+        complete_data.append(data.iloc[rows, cols])
+
+    if not all(df.notna().all().all() for df in complete_data):
+        raise ValueError("Bicliques contain missing elements")
+
+    return data, complete_data
